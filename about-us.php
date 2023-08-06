bc4b27247e4e1a45f3cef8e384d89aa5dd012a98
DeeptiGit22 <deeptigit22@gmail.com>
2023-08-06 21:19:44 +0530

    first commit

---
+++ about-us.php
@@ -0,0 +1,20 @@
+
+<?php include('partials-front/menu.php'); ?>
+
+<h2>Belly-Lovers</h2>
+ <section> 
+<p>Looking for an easy and convenient way to get your favorite food delivered right to your doorstep? Look no further than Belly-Lovers, the online food ordering system that makes it easy and hassle-free to get your meals delivered. With Belly-Lovers, you can order from your favorite restaurants and have your food delivered to your doorstep, no matter where you are in the world.
+
+Belly-Lovers is the perfect solution for those who want to enjoy their favorite food without having to leave their homes. With Belly-Lovers, you can order from a variety of different cuisines, including Chinese, Italian, Indian, Thai, and more
+</p>
+<br>
+<p>So what are you waiting for? Order your favorite food today with Belly-Lovers!</p>
+<br>
+<p> Pay only after getting your order and enjoy your food without any tension </p>
+<p> For Any Queries you can contact us on : <span> 9264983477 </span> </p>
+
+
+</section>
+
+
+<?php include('partials-front/footer.php'); ?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Belly-lovers. Developed By - <a href="#"> Deepti Singh</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
